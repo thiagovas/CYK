171ff5219549c0a5d3277a14abc8fa6778b44ce6
Thiago Vieira <thiagovieiraas@gmail.com>
2014-05-04 13:53:19 -0300

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include <cstring>
+#include <string>
+#include <locale>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
